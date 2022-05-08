f958a1ce7865487050748c48b232ad23ce62350f
Maks <semenov@mail.ru>
2022-05-09 01:51:11 +0400

    procedure генерация строки sql запроса

---
+++ back-end/Controllers/ProcedureControllers.cs
@@ -0,0 +1,31 @@
+﻿using back_end.Server;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+namespace back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcedureControllers : ControllerBase
+    {
+        private readonly ILogger<FileControllers> _logger;
+        private readonly ProcedureServer procedureServer = new ProcedureServer();
+        public ProcedureControllers(ILogger<FileControllers> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("/procedure")]
+        public string procedure(string name, dynamic json)
+        {
+            string sql = $"select * from {name}({ procedureServer.paramsGenerator(json) })";
+            return sql;
+        }
+    }
+
+}
+/*
+ {
+    "id": 2,
+    "name": "test"
+ }
+ */
---
+++ back-end/Server/ProcedureServer.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+namespace back_end.Server
+{
+    public class ProcedureServer
+    {
+        public string paramsGenerator(dynamic json)
+        {
+            string _params = "";
+            foreach (var k in json.EnumerateObject())
+            {
+                if (_params != "")
+                {
+                    _params += ", ";
+                }
+                dynamic value = checkParams(k.Value);
+                _params += $"{k.Name} => {value}";
+            }
+            return _params;
+        }
+
+        dynamic checkParams(dynamic value)
+        {
+            switch (value.ValueKind)
+            {
+                case JsonValueKind.String:
+                    return checkStringJson(value);
+                default:
+                    return value;
+            }
+        }
+        dynamic checkStringJson(dynamic value)
+        {
+            if (value.ValueKind == JsonValueKind.String)
+            {
+                value = $"\"{value}\"";
+            }
+            return value;
+        }
+    }
+}
